067a447f51834ca22bb658ecab1603e494119f01
jtabilas <patricktabilas@gmail.com>
2023-07-14 13:05:28 +0200

    fix CommessaService

--- commessa/src/main/java/com/example/commessa/service/CommessaService.java
+++ commessa/src/main/java/com/example/commessa/service/CommessaService.java
@@ -1,17 +1,54 @@
 package com.example.commessa.service;
+
 import com.example.commessa.model.Commessa;
+import com.example.commessa.repository.CommessaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface CommessaService {
-    // save operation
-    Commessa saveCommessa(Commessa commessa);
-    // read operation
-    List<Commessa> fetchCommessa();
-    Commessa getCommessa(Integer id);
+@Service
+public class CommessaService {
 
-    // update operation
-    Commessa updateComessa(Commessa commessa, Integer id);
-    // delete operation
-    void deleteCommessa(Integer id);
+    @Autowired
+    private  CommessaRepository commessaRepository;
+
+    // Save operation
+    public Commessa saveCommessa(Commessa commessa){
+        return commessaRepository.save(commessa);
+    }
+
+    // Read operation
+    public List<Commessa>  fetchCommessa(){
+        return commessaRepository.findAll();
+    }
+
+    // Read operation by id
+    public Commessa getCommessa(Integer id){
+        return commessaRepository.findById(id).orElseThrow();
+    }
+
+    // Update operation
+    public Commessa updateComessa(Commessa commessa, Integer id){
+        Commessa commessaToUpdate = commessaRepository.findById(id).orElseThrow();
+
+        commessaToUpdate.setDataOraAggiornamento(commessa.getDataOraAggiornamento());
+        commessaToUpdate.setDataOraCreazione(commessa.getDataOraCreazione());
+        commessaToUpdate.setDescrizione(commessa.getDescrizione());
+        commessaToUpdate.setTitolo(commessa.getTitolo());
+        commessaToUpdate.setIdUtente(commessa.getIdUtente());
+        commessaToUpdate.setNote(commessa.getNote());
+        commessaToUpdate.setIdOrganizzazioneCliente(commessa.getIdOrganizzazioneCliente());
+        commessaToUpdate.setIdUtente(commessa.getIdUtente());
+        commessaToUpdate.setIdRifCliente(commessa.getIdRifCliente());
+
+        return commessaRepository.save(commessaToUpdate);
+    }
+
+    // Delete operation
+    public void deleteCommessa(Integer id){
+        commessaRepository.deleteById(id);
+    }
+
+
 }
